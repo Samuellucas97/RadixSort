94caafc860dd726565fb592efd2df8e3cf7ccf56
Samuel <samuel1797@gmail.com>
2018-05-03 22:50:32 -0300

    add

--- src/radixSort.cpp
+++ src/radixSort.cpp
@@ -1,12 +1,35 @@
-#include "radix_sort.h"
-#include <memory>
-#include <iostream>
 
+/**
+ * @file    	radixSort.h
+ * @brief   	Contém a implementação função que ordena o vetor usando a estratégia do radix sort
+ * @author  	Samuel Lucas de Moura Ferino
+ * @since   	02.10.2017
+ * @version		0.0.10
+ */
+
+/// BIBLIOTECAS USADAS (STL)
+
+//#include <memory> 	// std::unique_ptr
+
+/// BIBLIOTECAS PRÓPRIAS
+
+#include "radixSort.h"
+
+
+/**
+ * @brief   Ordena o vetor usando a estratégia do radix sort
+ * @param   A   Vetor a ser ordenado
+ * @param   l   Início do vetor 
+ * @param   r   Fim do vetor
+ */
 void radixsort( unsigned int A[], int l, int r )
 {
+	/*!	\var	unsigned int maior_elemento	
+		\brief	Maior elemento */
 	unsigned int maior_elemento = A[0];
 	int count, i = 0;
 	unsigned int k;
+
 	int ordenacao_comecando_pelas_unidades = 1;
 
 	for(count = l; count < r; ++count )
